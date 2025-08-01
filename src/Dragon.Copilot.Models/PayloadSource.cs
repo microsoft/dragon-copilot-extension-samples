9898920ceea3833342c5fc6fa5721d7bdc560e1c
brandonpollett <brpoll@microsoft.com>
2025-08-01 12:30:16 -0400

    Fix copyright header issues.

--- src/Dragon.Copilot.Models/PayloadSource.cs
+++ src/Dragon.Copilot.Models/PayloadSource.cs
@@ -1,7 +1,7 @@
-using System;
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.Text.Json.Serialization;
 
 namespace Dragon.Copilot.Models;
