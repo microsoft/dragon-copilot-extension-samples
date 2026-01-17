63cd1e17ebca5ef4844bd332a2804e88911836f2
Timothy Lee <timothy_lee@nuance.com>
2026-01-17 15:24:57 -0500

    ADO2563922 - Updated Swashbuckle.AspNetCore to version 10.1.0 in response to a Dependabot request. This upgrade required additional code changes beyond a simple version bump, so a new pull request was created. The direct reference to Microsoft.AspNetCore.OpenApi was removed because it is already included transitively by Swashbuckle.AspNetCore.

--- samples/DragonCopilot/Workflow/SampleExtension.Web/Extensions/WebApplicationExtensions.cs
+++ samples/DragonCopilot/Workflow/SampleExtension.Web/Extensions/WebApplicationExtensions.cs
@@ -2,10 +2,12 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using SampleExtension.Web.Configuration;
 using SampleExtension.Web.Middleware;
 
@@ -64,19 +66,14 @@ internal static class WebApplicationExtensions
                     BearerFormat = "JWT"
                 });
 
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                c.AddSecurityRequirement(document =>
                 {
+                    OpenApiSecuritySchemeReference schemeRef = new("Bearer", document);
+
+                    return new OpenApiSecurityRequirement
                     {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
+                        [schemeRef] = new List<string>()
+                    };
                 });
             }
 
@@ -92,19 +89,14 @@ internal static class WebApplicationExtensions
                     Type = SecuritySchemeType.ApiKey
                 });
 
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                c.AddSecurityRequirement(document =>
                 {
+                    OpenApiSecuritySchemeReference schemeRef = new("LicenseKey", document);
+
+                    return new OpenApiSecurityRequirement
                     {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "LicenseKey"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
+                        [schemeRef] = new List<string>()
+                    };
                 });
             }
         });
